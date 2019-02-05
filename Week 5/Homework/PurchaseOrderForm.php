9cbb91472a695c137c1f76101d4db01935ed1b22
David Myers <davidmyers9000@gmail.com>
2019-02-05 18:41:33 -0500

    hooked up 3 inputs to message page

--- Week 5/Homework/PurchaseOrderForm.php
+++ Week 5/Homework/PurchaseOrderForm.php
@@ -2,7 +2,15 @@
 <head></head>
 <body>
    <form action="PurchaseOrderMessage.php" method="post">
-      Enter your message: <input type="text" name="msg" size="30">
+      Quantity: <br>
+      <input type="text" name="quantity" size="30"><br>
+      <br>
+      Price: <br>
+      <input type="text" name="price" size="30"><br>
+      <br>
+      Tax Rate: <br>
+      <input type="text" name="tax_rate" size="30"><br>
+      <br>
       <input type="submit" name="submit" value="Send">
    </form>
 </body>
--- Week 5/Homework/PurchaseOrderMessage.php
+++ Week 5/Homework/PurchaseOrderMessage.php
@@ -3,8 +3,12 @@
 <body>
 <?php
    // retrieve form data
-   $input = $_POST['msg'];
-   echo "You said: <i>$input</i>";
+   $input1 = $_POST['quantity'];
+   $input2 = $_POST['price'];
+   $input3 = $_POST['tax_rate'];
+   echo "Quantity: <em>$input1</em><br>";
+   echo "Price: <em>$input2</em><br>";
+   echo "Tax Rate: <em>$input3</em><br>";
 ?>
 </body>
 </html>
