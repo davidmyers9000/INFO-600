bb9be12e2bc8a6818e2adae954ba9e8d841329f8
David Myers <davidmyers9000@gmail.com>
2019-02-05 19:18:01 -0500

    added subtotal, tax amount, purchase total calculations, and table html/css

--- Week 5/Homework/PurchaseOrderMessage.php
+++ Week 5/Homework/PurchaseOrderMessage.php
@@ -1,14 +1,50 @@
 <html>
-<head></head>
+<head>
+  <style>
+    table {
+      border-collapse: collapse;
+    }
+    thead {
+      background-color: lightblue;
+    }
+    table, td, th {
+      padding: .625em;
+      border: .063em solid black;
+      text-align: center;
+    }
+  </style>
+</head>
 <body>
-<?php
-   // retrieve form data
-   $input1 = $_POST['quantity'];
-   $input2 = $_POST['price'];
-   $input3 = $_POST['tax_rate'];
-   echo "Quantity: <em>$input1</em><br>";
-   echo "Price: <em>$input2</em><br>";
-   echo "Tax Rate: <em>$input3</em><br>";
-?>
+  <?php
+  // retrieve form data
+  $quantity = $_POST['quantity'];
+  $price = $_POST['price'];
+  $subtotal = $price * $quantity;
+  $tax_rate = $_POST['tax_rate'];
+  $tax_amount = $subtotal * $tax_rate;
+  $total = $subtotal + $tax_amount;
+
+  ?>
+
+  <table>
+      <thead>
+        <tr>
+           <td>Quantity</td>
+           <td>Price</td>
+           <td>Subtotal</td>
+           <td>Tax<?php echo ' <br>('.($tax_rate*100).'%)' ?> </td>
+           <td>Purchase Total</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><?php echo $quantity ?></td>
+          <td><?php echo number_format($price, 2, ".", "") ?></td>
+          <td><?php echo number_format($subtotal, 2, ".", "") ?></td>
+          <td><?php echo number_format($subtotal * $tax_rate, 2, ".", "") ?></td></td>
+          <td><?php echo number_format($total, 2, ".", "") ?></td>
+        </tr>
+      </tbody>
+    </table>
 </body>
 </html>
